43db0118c5d3054c8fb7bf91e27cbe63cf89ba18
Zeck Li <devformatters@gmail.com>
2019-10-28 16:52:07 +0800

    Alter apollo gql demo template.

--- demo/apollo/index.tsx
+++ demo/apollo/index.tsx
@@ -5,60 +5,63 @@ import { ApolloClient } from 'apollo-client'
 import gql from 'graphql-tag'
 import { ApolloProvider } from 'react-apollo'
 
-const inMemoryCache = new InMemoryCache()
+/**
+ * Template for ApolloGQL environment.
+ *
+ */
 
-export const apollo = new ApolloClient({
-  cache: inMemoryCache,
-  link: createUploadLink({
-    uri: 'localhost:4000',
-    headers: {}
-  })
-})
-
-export const DemoGQLQuery = gql`
+const DemoGQLQuery = gql`
   query DemoQuery() {
 
   }
 `
 
-export const DemoQuery = () => {
-  const { data, loading, error } = useQuery(DemoGQLQuery, { variables: {} })
-  if (!data || loading) {
-    return null
-  }
-  return <DemoSearch data={data} />
-}
-
-export const DemoGQLSearch = gql`
+const DemoGQLSearch = gql`
   query DemoSearch() {
 
   }
 `
 
-export const DemoSearch = (props: any) => {
-  const [search, searchResult] = useLazyQuery(DemoGQLSearch, { variables: {} })
-  const newProps = { ...props, search, searchResult }
-  return <DemoMutation {...newProps} />
-}
-
-export const DemoGQLUpdate = gql`
-  mutation DemoMutation() {
+const DemoGQLUpdate = gql`
+  mutation DemoUpdate() {
 
   }
 `
 
-export const DemoGQLUpload = gql`
+const DemoGQLUpload = gql`
   mutation DemoUplod() {
 
   }
 `
 
-export const DemoMutation = (props: any) => {
-  const [update] = useMutation(DemoGQLUpdate, { variables: {} })
-  const [upload] = useMutation(DemoGQLUpload, { variables: {} })
-  const newProps = { ...props, update, upload }
-  return <>{() => newProps.children(newProps)}</>
+const DemoQuery = () => {
+  const { data, loading, error } = useQuery(DemoGQLQuery, { variables: {} })
+  if (!data || loading) {
+    return null
+  }
+  return <></>
 }
+
+const DemoSearch = (props: any) => {
+  const [search, searchResult] = useLazyQuery(DemoGQLSearch)
+  const newProps = { ...props, search, searchResult }
+  return <></>
+}
+
+const DemoMutation = (props: any) => {
+  const [update] = useMutation(DemoGQLUpdate)
+  const [upload] = useMutation(DemoGQLUpload)
+  const newProps = { ...props, update, upload }
+  return <></>
+}
+
+export const apollo = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: createUploadLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {}
+  })
+})
 
 export const ApolloDemoApp = ({ children }) => {
   return <ApolloProvider client={apollo}>{children}</ApolloProvider>
