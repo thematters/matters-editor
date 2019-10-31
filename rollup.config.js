cc17306c6614b87e4f345dbdba690fcaf4a7f5f6
Zeck Li <devformatters@gmail.com>
2019-10-31 23:11:23 +0800

    Change config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,35 @@
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+
+import pkg from './package.json'
 
 const inputDir = 'src/themes'
 
 const outputDir = 'build/themes'
 
 export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    plugins: [
+      commonjs({ include: 'node_modules/**'}),
+      external(),
+      resolve(),
+      typescript({ clean: true })
+    ]
+  },
   {
     input: `${inputDir}/editor.css`,
     output: [
