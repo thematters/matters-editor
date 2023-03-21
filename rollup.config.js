a221e169b0b251f59a4b86024489ba48b734c719
robertu <4065233+robertu7@users.noreply.github.com>
2023-03-21 21:17:49 +0700

    feat: use rollup instead of vite

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import external from 'rollup-plugin-peer-deps-external'
+
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: packageJson.name,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/src/index.d.ts',
+    output: [{ file: packageJson.types, format: 'esm' }],
+    plugins: [dts.default()],
+  },
+]
