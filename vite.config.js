72bb497146b1cdea55f7ab1abc3f52f09961c6f1
robertu <4065233+robertu7@users.noreply.github.com>
2023-03-21 16:05:26 +0700

    fix: remove React deps in bundle

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 /// <reference types="vitest" />
 // Configure Vitest (https://vitest.dev/config/)
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+
+import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'main',
+      name: 'matters-editor',
       fileName: 'main',
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
   plugins: [react(), dts()],
