0af6d969efc3019f7a1c1ca57823c7371599a584
robertu <4065233+robertu7@users.noreply.github.com>
2023-06-02 10:17:19 +0800

    feat: custom HOrizontalRule

---
+++ src/editors/extensions/horizontalRule.ts
@@ -0,0 +1,63 @@
+import { mergeAttributes, Node, nodeInputRule } from '@tiptap/core'
+import { TextSelection } from '@tiptap/pm/state'
+
+export interface HorizontalRuleOptions {
+  HTMLAttributes: Record<string, any>
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    horizontalRule: {
+      /**
+       * Add a horizontal rule
+       */
+      setHorizontalRule: () => ReturnType
+    }
+  }
+}
+
+export const HorizontalRule = Node.create<HorizontalRuleOptions>({
+  name: 'horizontalRule',
+
+  addOptions() {
+    return {
+      HTMLAttributes: {},
+    }
+  },
+
+  group: 'block',
+
+  parseHTML() {
+    return [{ tag: 'hr' }]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['hr', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)]
+  },
+
+  addCommands() {
+    return {
+      setHorizontalRule:
+        () =>
+        ({ chain }) => {
+          return chain()
+            .insertContent([
+              { type: this.name },
+              {
+                type: 'paragraph',
+              },
+            ])
+            .run()
+        },
+    }
+  },
+
+  addInputRules() {
+    return [
+      nodeInputRule({
+        find: /^(?:---|—-|___\s|\*\*\*\s)$/,
+        type: this.type,
+      }),
+    ]
+  },
+})
