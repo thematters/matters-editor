f63c57540d2707e7ebcc0f4439b51197bd88d004
robertu <4065233+robertu7@users.noreply.github.com>
2024-06-13 21:40:47 +0800

    feat: allow to truncate long link text and remove protocol

--- src/transformers/normalize.ts
+++ src/transformers/normalize.ts
@@ -1,6 +1,7 @@
 import type { Extensions } from '@tiptap/core'
 import { getSchema } from '@tiptap/core'
 import { DOMParser, DOMSerializer, Node } from '@tiptap/pm/model'
+import isURL from 'validator/lib/isURL'
 import { createHTMLDocument, parseHTML, type VHTMLDocument } from 'zeed-dom'
 
 import {
@@ -9,6 +10,13 @@ import {
   makeJournalEditorExtensions,
   Mention,
 } from '../editors/extensions'
+
+export type NormalizeOptions = {
+  truncate?: {
+    maxLength: number
+    keepProtocol: boolean
+  }
+}
 
 export const makeNormalizer = (extensions: Extensions) => {
   const schema = getSchema(extensions)
@@ -30,20 +38,80 @@ export const makeNormalizer = (extensions: Extensions) => {
   }
 }
 
-export const normalizeArticleHTML = (html: string): string => {
+// match HTML anchor tags and truncate the text
+export const truncateLinkText = (
+  html: string,
+  { maxLength, keepProtocol }: { maxLength: number; keepProtocol: boolean },
+): string => {
+  const regex = /<a\s+([^>]*?)>(.*?)<\/a>/gi
+
+  return html.replace(regex, (match, attributes: string, text: string) => {
+    if (!isURL(text)) {
+      return match
+    }
+
+    let truncatedText = text
+
+    if (!keepProtocol) {
+      truncatedText = text.replace(/(^\w+:|^)\/\//, '')
+    }
+
+    if (maxLength === 0) {
+      throw new Error('maxLength must be greater than 0')
+    }
+
+    if (maxLength && truncatedText.length > maxLength) {
+      truncatedText = truncatedText.slice(0, maxLength) + '...'
+    }
+
+    return `<a ${attributes}>${truncatedText}</a>`
+  })
+}
+
+export const normalizeArticleHTML = (
+  html: string,
+  options?: NormalizeOptions,
+): string => {
   const extensions = makeArticleEditorExtensions({})
   const normalizer = makeNormalizer([...extensions, Mention])
-  return normalizer(html)
+
+  let normalizedHtml = normalizer(html)
+
+  if (options?.truncate) {
+    normalizedHtml = truncateLinkText(html, options.truncate)
+  }
+
+  return normalizedHtml
 }
 
-export const normalizeCommentHTML = (html: string): string => {
+export const normalizeCommentHTML = (
+  html: string,
+  options?: NormalizeOptions,
+): string => {
   const extensions = makeCommentEditorExtensions({})
   const normalizer = makeNormalizer([...extensions, Mention])
-  return normalizer(html)
+
+  let normalizedHtml = normalizer(html)
+
+  if (options?.truncate) {
+    normalizedHtml = truncateLinkText(html, options.truncate)
+  }
+
+  return normalizedHtml
 }
 
-export const normalizeJournalHTML = (html: string): string => {
+export const normalizeJournalHTML = (
+  html: string,
+  options?: NormalizeOptions,
+): string => {
   const extensions = makeJournalEditorExtensions({})
   const normalizer = makeNormalizer([...extensions, Mention])
-  return normalizer(html)
+
+  let normalizedHtml = normalizer(html)
+
+  if (options?.truncate) {
+    normalizedHtml = truncateLinkText(html, options.truncate)
+  }
+
+  return normalizedHtml
 }
