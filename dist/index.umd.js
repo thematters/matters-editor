8602561fc0dac8bb20ac4b789bcb9f40e44967b0
robertu7 <robertu7@users.noreply.github.com>
2024-06-14 01:37:03 +0000

    deploy: 181e8fc07dbaaba62186b284946782e1044d3649

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -27691,7 +27691,7 @@ img.ProseMirror-separator {
      * @param {string} [space]
      * @returns {Schema}
      */
-    function merge(definitions, space) {
+    function merge$1(definitions, space) {
       /** @type {Properties} */
       const property = {};
       /** @type {Normal} */
@@ -28897,8 +28897,8 @@ img.ProseMirror-separator {
      * @typedef {import('./lib/util/schema.js').Schema} Schema
      */
 
-    const html$5 = merge([xml, xlink, xmlns, aria$1, html$6], 'html');
-    const svg = merge([xml, xlink, xmlns, aria$1, svg$1], 'svg');
+    const html$5 = merge$1([xml, xlink, xmlns, aria$1, html$6], 'html');
+    const svg = merge$1([xml, xlink, xmlns, aria$1, svg$1], 'svg');
 
     /**
      * @typedef Options
@@ -71735,6 +71735,370 @@ img.ProseMirror-separator {
         return String(result);
     };
 
+    var isURL$1 = {exports: {}};
+
+    var assertString = {exports: {}};
+
+    (function (module, exports) {
+
+    	Object.defineProperty(exports, "__esModule", {
+    	  value: true
+    	});
+    	exports.default = assertString;
+    	function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+    	function assertString(input) {
+    	  var isString = typeof input === 'string' || input instanceof String;
+    	  if (!isString) {
+    	    var invalidType = _typeof(input);
+    	    if (input === null) invalidType = 'null';else if (invalidType === 'object') invalidType = input.constructor.name;
+    	    throw new TypeError("Expected a string but received a ".concat(invalidType));
+    	  }
+    	}
+    	module.exports = exports.default;
+    	module.exports.default = exports.default; 
+    } (assertString, assertString.exports));
+
+    var assertStringExports = assertString.exports;
+
+    var isFQDN = {exports: {}};
+
+    var merge = {exports: {}};
+
+    (function (module, exports) {
+
+    	Object.defineProperty(exports, "__esModule", {
+    	  value: true
+    	});
+    	exports.default = merge;
+    	function merge() {
+    	  var obj = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    	  var defaults = arguments.length > 1 ? arguments[1] : undefined;
+    	  for (var key in defaults) {
+    	    if (typeof obj[key] === 'undefined') {
+    	      obj[key] = defaults[key];
+    	    }
+    	  }
+    	  return obj;
+    	}
+    	module.exports = exports.default;
+    	module.exports.default = exports.default; 
+    } (merge, merge.exports));
+
+    var mergeExports = merge.exports;
+
+    (function (module, exports) {
+
+    	Object.defineProperty(exports, "__esModule", {
+    	  value: true
+    	});
+    	exports.default = isFQDN;
+    	var _assertString = _interopRequireDefault(assertStringExports);
+    	var _merge = _interopRequireDefault(mergeExports);
+    	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    	var default_fqdn_options = {
+    	  require_tld: true,
+    	  allow_underscores: false,
+    	  allow_trailing_dot: false,
+    	  allow_numeric_tld: false,
+    	  allow_wildcard: false,
+    	  ignore_max_length: false
+    	};
+    	function isFQDN(str, options) {
+    	  (0, _assertString.default)(str);
+    	  options = (0, _merge.default)(options, default_fqdn_options);
+
+    	  /* Remove the optional trailing dot before checking validity */
+    	  if (options.allow_trailing_dot && str[str.length - 1] === '.') {
+    	    str = str.substring(0, str.length - 1);
+    	  }
+
+    	  /* Remove the optional wildcard before checking validity */
+    	  if (options.allow_wildcard === true && str.indexOf('*.') === 0) {
+    	    str = str.substring(2);
+    	  }
+    	  var parts = str.split('.');
+    	  var tld = parts[parts.length - 1];
+    	  if (options.require_tld) {
+    	    // disallow fqdns without tld
+    	    if (parts.length < 2) {
+    	      return false;
+    	    }
+    	    if (!options.allow_numeric_tld && !/^([a-z\u00A1-\u00A8\u00AA-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}|xn[a-z0-9-]{2,})$/i.test(tld)) {
+    	      return false;
+    	    }
+
+    	    // disallow spaces
+    	    if (/\s/.test(tld)) {
+    	      return false;
+    	    }
+    	  }
+
+    	  // reject numeric TLDs
+    	  if (!options.allow_numeric_tld && /^\d+$/.test(tld)) {
+    	    return false;
+    	  }
+    	  return parts.every(function (part) {
+    	    if (part.length > 63 && !options.ignore_max_length) {
+    	      return false;
+    	    }
+    	    if (!/^[a-z_\u00a1-\uffff0-9-]+$/i.test(part)) {
+    	      return false;
+    	    }
+
+    	    // disallow full-width chars
+    	    if (/[\uff01-\uff5e]/.test(part)) {
+    	      return false;
+    	    }
+
+    	    // disallow parts starting or ending with hyphen
+    	    if (/^-|-$/.test(part)) {
+    	      return false;
+    	    }
+    	    if (!options.allow_underscores && /_/.test(part)) {
+    	      return false;
+    	    }
+    	    return true;
+    	  });
+    	}
+    	module.exports = exports.default;
+    	module.exports.default = exports.default; 
+    } (isFQDN, isFQDN.exports));
+
+    var isFQDNExports = isFQDN.exports;
+
+    var isIP = {exports: {}};
+
+    (function (module, exports) {
+
+    	Object.defineProperty(exports, "__esModule", {
+    	  value: true
+    	});
+    	exports.default = isIP;
+    	var _assertString = _interopRequireDefault(assertStringExports);
+    	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    	/**
+    	11.3.  Examples
+
+    	   The following addresses
+
+    	             fe80::1234 (on the 1st link of the node)
+    	             ff02::5678 (on the 5th link of the node)
+    	             ff08::9abc (on the 10th organization of the node)
+
+    	   would be represented as follows:
+
+    	             fe80::1234%1
+    	             ff02::5678%5
+    	             ff08::9abc%10
+
+    	   (Here we assume a natural translation from a zone index to the
+    	   <zone_id> part, where the Nth zone of any scope is translated into
+    	   "N".)
+
+    	   If we use interface names as <zone_id>, those addresses could also be
+    	   represented as follows:
+
+    	            fe80::1234%ne0
+    	            ff02::5678%pvc1.3
+    	            ff08::9abc%interface10
+
+    	   where the interface "ne0" belongs to the 1st link, "pvc1.3" belongs
+    	   to the 5th link, and "interface10" belongs to the 10th organization.
+    	 * * */
+    	var IPv4SegmentFormat = '(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
+    	var IPv4AddressFormat = "(".concat(IPv4SegmentFormat, "[.]){3}").concat(IPv4SegmentFormat);
+    	var IPv4AddressRegExp = new RegExp("^".concat(IPv4AddressFormat, "$"));
+    	var IPv6SegmentFormat = '(?:[0-9a-fA-F]{1,4})';
+    	var IPv6AddressRegExp = new RegExp('^(' + "(?:".concat(IPv6SegmentFormat, ":){7}(?:").concat(IPv6SegmentFormat, "|:)|") + "(?:".concat(IPv6SegmentFormat, ":){6}(?:").concat(IPv4AddressFormat, "|:").concat(IPv6SegmentFormat, "|:)|") + "(?:".concat(IPv6SegmentFormat, ":){5}(?::").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,2}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){4}(?:(:").concat(IPv6SegmentFormat, "){0,1}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,3}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){3}(?:(:").concat(IPv6SegmentFormat, "){0,2}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,4}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){2}(?:(:").concat(IPv6SegmentFormat, "){0,3}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,5}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){1}(?:(:").concat(IPv6SegmentFormat, "){0,4}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,6}|:)|") + "(?::((?::".concat(IPv6SegmentFormat, "){0,5}:").concat(IPv4AddressFormat, "|(?::").concat(IPv6SegmentFormat, "){1,7}|:))") + ')(%[0-9a-zA-Z-.:]{1,})?$');
+    	function isIP(str) {
+    	  var version = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+    	  (0, _assertString.default)(str);
+    	  version = String(version);
+    	  if (!version) {
+    	    return isIP(str, 4) || isIP(str, 6);
+    	  }
+    	  if (version === '4') {
+    	    return IPv4AddressRegExp.test(str);
+    	  }
+    	  if (version === '6') {
+    	    return IPv6AddressRegExp.test(str);
+    	  }
+    	  return false;
+    	}
+    	module.exports = exports.default;
+    	module.exports.default = exports.default; 
+    } (isIP, isIP.exports));
+
+    var isIPExports = isIP.exports;
+
+    (function (module, exports) {
+
+    	Object.defineProperty(exports, "__esModule", {
+    	  value: true
+    	});
+    	exports.default = isURL;
+    	var _assertString = _interopRequireDefault(assertStringExports);
+    	var _isFQDN = _interopRequireDefault(isFQDNExports);
+    	var _isIP = _interopRequireDefault(isIPExports);
+    	var _merge = _interopRequireDefault(mergeExports);
+    	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    	function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+    	function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+    	function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+    	function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+    	function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+    	function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+    	/*
+    	options for isURL method
+
+    	require_protocol - if set as true isURL will return false if protocol is not present in the URL
+    	require_valid_protocol - isURL will check if the URL's protocol is present in the protocols option
+    	protocols - valid protocols can be modified with this option
+    	require_host - if set as false isURL will not check if host is present in the URL
+    	require_port - if set as true isURL will check if port is present in the URL
+    	allow_protocol_relative_urls - if set as true protocol relative URLs will be allowed
+    	validate_length - if set as false isURL will skip string length validation (IE maximum is 2083)
+
+    	*/
+
+    	var default_url_options = {
+    	  protocols: ['http', 'https', 'ftp'],
+    	  require_tld: true,
+    	  require_protocol: false,
+    	  require_host: true,
+    	  require_port: false,
+    	  require_valid_protocol: true,
+    	  allow_underscores: false,
+    	  allow_trailing_dot: false,
+    	  allow_protocol_relative_urls: false,
+    	  allow_fragments: true,
+    	  allow_query_components: true,
+    	  validate_length: true
+    	};
+    	var wrapped_ipv6 = /^\[([^\]]+)\](?::([0-9]+))?$/;
+    	function isRegExp(obj) {
+    	  return Object.prototype.toString.call(obj) === '[object RegExp]';
+    	}
+    	function checkHost(host, matches) {
+    	  for (var i = 0; i < matches.length; i++) {
+    	    var match = matches[i];
+    	    if (host === match || isRegExp(match) && match.test(host)) {
+    	      return true;
+    	    }
+    	  }
+    	  return false;
+    	}
+    	function isURL(url, options) {
+    	  (0, _assertString.default)(url);
+    	  if (!url || /[\s<>]/.test(url)) {
+    	    return false;
+    	  }
+    	  if (url.indexOf('mailto:') === 0) {
+    	    return false;
+    	  }
+    	  options = (0, _merge.default)(options, default_url_options);
+    	  if (options.validate_length && url.length >= 2083) {
+    	    return false;
+    	  }
+    	  if (!options.allow_fragments && url.includes('#')) {
+    	    return false;
+    	  }
+    	  if (!options.allow_query_components && (url.includes('?') || url.includes('&'))) {
+    	    return false;
+    	  }
+    	  var protocol, auth, host, hostname, port, port_str, split, ipv6;
+    	  split = url.split('#');
+    	  url = split.shift();
+    	  split = url.split('?');
+    	  url = split.shift();
+    	  split = url.split('://');
+    	  if (split.length > 1) {
+    	    protocol = split.shift().toLowerCase();
+    	    if (options.require_valid_protocol && options.protocols.indexOf(protocol) === -1) {
+    	      return false;
+    	    }
+    	  } else if (options.require_protocol) {
+    	    return false;
+    	  } else if (url.slice(0, 2) === '//') {
+    	    if (!options.allow_protocol_relative_urls) {
+    	      return false;
+    	    }
+    	    split[0] = url.slice(2);
+    	  }
+    	  url = split.join('://');
+    	  if (url === '') {
+    	    return false;
+    	  }
+    	  split = url.split('/');
+    	  url = split.shift();
+    	  if (url === '' && !options.require_host) {
+    	    return true;
+    	  }
+    	  split = url.split('@');
+    	  if (split.length > 1) {
+    	    if (options.disallow_auth) {
+    	      return false;
+    	    }
+    	    if (split[0] === '') {
+    	      return false;
+    	    }
+    	    auth = split.shift();
+    	    if (auth.indexOf(':') >= 0 && auth.split(':').length > 2) {
+    	      return false;
+    	    }
+    	    var _auth$split = auth.split(':'),
+    	      _auth$split2 = _slicedToArray(_auth$split, 2),
+    	      user = _auth$split2[0],
+    	      password = _auth$split2[1];
+    	    if (user === '' && password === '') {
+    	      return false;
+    	    }
+    	  }
+    	  hostname = split.join('@');
+    	  port_str = null;
+    	  ipv6 = null;
+    	  var ipv6_match = hostname.match(wrapped_ipv6);
+    	  if (ipv6_match) {
+    	    host = '';
+    	    ipv6 = ipv6_match[1];
+    	    port_str = ipv6_match[2] || null;
+    	  } else {
+    	    split = hostname.split(':');
+    	    host = split.shift();
+    	    if (split.length) {
+    	      port_str = split.join(':');
+    	    }
+    	  }
+    	  if (port_str !== null && port_str.length > 0) {
+    	    port = parseInt(port_str, 10);
+    	    if (!/^[0-9]+$/.test(port_str) || port <= 0 || port > 65535) {
+    	      return false;
+    	    }
+    	  } else if (options.require_port) {
+    	    return false;
+    	  }
+    	  if (options.host_whitelist) {
+    	    return checkHost(host, options.host_whitelist);
+    	  }
+    	  if (host === '' && !options.require_host) {
+    	    return true;
+    	  }
+    	  if (!(0, _isIP.default)(host) && !(0, _isFQDN.default)(host, options) && (!ipv6 || !(0, _isIP.default)(ipv6, 6))) {
+    	    return false;
+    	  }
+    	  host = host || ipv6;
+    	  if (options.host_blacklist && checkHost(host, options.host_blacklist)) {
+    	    return false;
+    	  }
+    	  return true;
+    	}
+    	module.exports = exports.default;
+    	module.exports.default = exports.default; 
+    } (isURL$1, isURL$1.exports));
+
+    var isURLExports = isURL$1.exports;
+    var isURL = /*@__PURE__*/getDefaultExportFromCjs(isURLExports);
+
     var SelectorType;
     (function (SelectorType) {
         SelectorType["Attribute"] = "attribute";
@@ -73149,20 +73513,53 @@ img.ProseMirror-separator {
             return document.render();
         };
     };
-    var normalizeArticleHTML = function (html) {
+    // match HTML anchor tags and truncate the text
+    var truncateLinkText = function (html, _a) {
+        var maxLength = _a.maxLength, keepProtocol = _a.keepProtocol;
+        var regex = /<a\s+([^>]*?)>(.*?)<\/a>/gi;
+        return html.replace(regex, function (match, attributes, text) {
+            if (!isURL(text)) {
+                return match;
+            }
+            var truncatedText = text;
+            if (!keepProtocol) {
+                truncatedText = text.replace(/(^\w+:|^)\/\//, '');
+            }
+            if (maxLength <= 0) {
+                throw new Error('maxLength must be greater than 0');
+            }
+            if (maxLength && truncatedText.length > maxLength) {
+                truncatedText = truncatedText.slice(0, maxLength) + '...';
+            }
+            return "<a ".concat(attributes, ">").concat(truncatedText, "</a>");
+        });
+    };
+    var normalizeArticleHTML = function (html, options) {
         var extensions = makeArticleEditorExtensions({});
         var normalizer = makeNormalizer(__spreadArray(__spreadArray([], extensions, true), [Mention], false));
-        return normalizer(html);
+        var normalizedHtml = normalizer(html);
+        if (options === null || options === void 0 ? void 0 : options.truncate) {
+            normalizedHtml = truncateLinkText(html, options.truncate);
+        }
+        return normalizedHtml;
     };
-    var normalizeCommentHTML = function (html) {
+    var normalizeCommentHTML = function (html, options) {
         var extensions = makeCommentEditorExtensions({});
         var normalizer = makeNormalizer(__spreadArray(__spreadArray([], extensions, true), [Mention], false));
-        return normalizer(html);
+        var normalizedHtml = normalizer(html);
+        if (options === null || options === void 0 ? void 0 : options.truncate) {
+            normalizedHtml = truncateLinkText(html, options.truncate);
+        }
+        return normalizedHtml;
     };
-    var normalizeJournalHTML = function (html) {
+    var normalizeJournalHTML = function (html, options) {
         var extensions = makeJournalEditorExtensions({});
         var normalizer = makeNormalizer(__spreadArray(__spreadArray([], extensions, true), [Mention], false));
-        return normalizer(html);
+        var normalizedHtml = normalizer(html);
+        if (options === null || options === void 0 ? void 0 : options.truncate) {
+            normalizedHtml = truncateLinkText(html, options.truncate);
+        }
+        return normalizedHtml;
     };
 
     var isEmptyText = function (node) {
@@ -73419,6 +73816,7 @@ img.ProseMirror-separator {
     exports.textInputRule = textInputRule;
     exports.textPasteRule = textPasteRule;
     exports.textblockTypeInputRule = textblockTypeInputRule;
+    exports.truncateLinkText = truncateLinkText;
     exports.useArticleEdtor = useArticleEdtor;
     exports.useCommentEditor = useCommentEditor;
     exports.useCurrentEditor = useCurrentEditor;
--- dist/types/src/transformers/normalize.d.ts
+++ dist/types/src/transformers/normalize.d.ts
@@ -1,5 +1,15 @@
 import type { Extensions } from '@tiptap/core';
+export type NormalizeOptions = {
+    truncate?: {
+        maxLength: number;
+        keepProtocol: boolean;
+    };
+};
 export declare const makeNormalizer: (extensions: Extensions) => (html: string) => string;
-export declare const normalizeArticleHTML: (html: string) => string;
-export declare const normalizeCommentHTML: (html: string) => string;
-export declare const normalizeJournalHTML: (html: string) => string;
+export declare const truncateLinkText: (html: string, { maxLength, keepProtocol }: {
+    maxLength: number;
+    keepProtocol: boolean;
+}) => string;
+export declare const normalizeArticleHTML: (html: string, options?: NormalizeOptions) => string;
+export declare const normalizeCommentHTML: (html: string, options?: NormalizeOptions) => string;
+export declare const normalizeJournalHTML: (html: string, options?: NormalizeOptions) => string;
